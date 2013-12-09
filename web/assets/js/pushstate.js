cc3280e87d68dda73d8904d292dcc823798270e1
Romain Reynaud <rreynaud@kilix.fr>
2013-12-09 12:29:28 +0000

    Pushstate refacto

--- web/assets/js/pushstate.js
+++ web/assets/js/pushstate.js
@@ -8,45 +8,37 @@
     // Establish Variables
     var
         History = window.History, // Note: We are using a capital H instead of a lower h
-        State = History.getState();
-        Pos = $(".container").data('pos');
+        State = History.getState(),
+        currentPos,
+        newPos,
+        slideNext = true;
 
     function loadAjaxContent(url){
         /*REFACTORABLE*/
+        slideNext = newPos>currentPos ? true : false;
         $(".nav-links-wrapper").addClass('disabled');
-        $( ".main-wrapper" ).append( "<div class='wrapper2'></div>" );
-        $(".wrapper2").load(url+" .container", function(data){
-            var newPos = $(data).find(".container").data("pos");
-            if (newPos > Pos) 
-            {
-                $(".wrapper").transition({ x: '-100%' }, 1200);
-                $(".wrapper2").transition({ x: '0%' }, 1200, function(){
-                    $(".wrapper").remove();
-                    $(".wrapper2:first-child").removeClass('wrapper2').addClass('wrapper');
-                    $(".nav-links-wrapper").removeClass('disabled');
-                    Pos = $(".container").data('pos');
-               });
-            }
-            else
-            {
-                $(".wrapper2").removeClass('wrapper2').addClass('wrapper2rev');
-                $(".wrapper").transition({ x: '100%' }, 1200);
-                $(".wrapper2rev").transition({ x: '0%' }, 1200, function(){
-                    $(".wrapper").remove();
-                    $(".wrapper2rev:first-child").removeClass('wrapper2rev').addClass('wrapper');
-                    $(".nav-links-wrapper").removeClass('disabled');
-                    Pos = $(".container").data('pos');
-                });
-            }    
+        $( ".main-wrapper" ).append( "<div class='wrapper wrapper-new'></div>" );
+        if(!slideNext){$('.wrapper-new').addClass('wrapper-prev');}
+
+
+        $(".wrapper-new").load(url+" .container", function(data){
+            
+
+            $(".wrapper:first-child").transition({ x: slideNext?'-100%':'100%' }, 1200);
+            $(".wrapper-new").transition({ x: '0%' }, 1200, function(){
+                $(".wrapper:first-child").remove();
+                $(".wrapper-new").removeClass('wrapper-new');
+                $(".nav-links-wrapper").removeClass('disabled');
+                Pos = $(".container").data('pos');
+            });
+ 
         });
     }
-        
 
     function updateContent(State){
 
         loadAjaxContent(State.url);
     }
-
     // Bind to State Change
     History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
         // Log the State
@@ -57,8 +49,13 @@
 
     $(".nav-links-wrapper a").on("click", function(evt) {
             //Prevent the browsers default behaviour of navigating to the hyperlink
+            currentPos = $(".nav-link.current").data('pos');
+            newPos = $(this).data('pos');
+
+            $('.nav-link.current').removeClass('current');
+            $(this).addClass('current');
+
             History.pushState(null, this.textContent, this.href);
             evt.preventDefault();
        });
-
 })(window);
