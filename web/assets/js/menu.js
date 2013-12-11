5e4e88fc651270afd6838df86f1f8cfbd6dfb680
Benjamin De Bernardi <bdebernardi@kilix.fr>
2013-12-11 12:05:40 +0100

    MV transition navbar from body to .site-wrapper

--- web/assets/js/menu.js
+++ web/assets/js/menu.js
@@ -1,9 +1,9 @@
 $(function(){
 	$('.toggle-menu').on('click',function(){
 	$('.nav-links-wrapper').removeClass('nav-links-hidden');
-		$('body').toggleClass('unfolded');
+		$('.site-wrapper').toggleClass('unfolded');
 		window.setTimeout(function(){
-			if(!$('body').hasClass('unfolded')) {
+			if(!$('.site-wrapper').hasClass('unfolded')) {
 				$('.nav-links-wrapper').addClass('nav-links-hidden');
 			}
 		}
