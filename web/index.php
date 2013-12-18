93f9502b9820805dc52cfdb7cdf0cabc8755e9cf
Benjamin De Bernardi <bdebernardi@kilix.fr>
2013-12-18 18:13:04 +0100

    Fix pushstate transition + javascript Fix pushstate navlink current Fix position fixed on landing when for small-only

--- web/index.php
+++ web/index.php
@@ -11,32 +11,38 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('home.html.twig', array(
+        'page' => 'home',
     ));
 });
 
 $app->get('/team', function() use ($app) {
     return $app['twig']->render('team.html.twig', array(
+        'page' => 'team',
     ));
 });
 
 $app->get('/agilite', function() use ($app) {
     return $app['twig']->render('agilite.html.twig', array(
+        'page' => 'agilite',
     ));
 });
 
 $app->get('/contact', function() use ($app) {
     return $app['twig']->render('contact.html.twig', array(
+        'page' => 'contact',
     ));
 });
 
 // MODE DEBUG : Route for animation developpement, to delete when release come
 $app->get('/anim', function() use ($app) {
     return $app['twig']->render('animation.html.twig', array(
+        'page' => '',
     ));
 });
 
 $app->get('/anim2', function() use ($app) {
     return $app['twig']->render('animation2.html.twig', array(
+        'page' => '',
     ));
 });
 
