d5246c844e8dd50e4dce8149d5718be8686deaed
Sylvain Arrambourg <sarrambourg@kilix.fr>
2014-01-08 13:44:09 +0100

    Add composer tasks to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :log_level, :info
 set :format, :pretty
 set :pty, true
 
+set :linked_dirs, %w{vendor}
+
 namespace :deploy do
 
   desc 'Overwrite deploy:restart (useless)'
@@ -18,6 +20,28 @@ namespace :deploy do
     end
   end
 
-  after :finishing, 'deploy:cleanup'
+  after :finishing, 'composer:install', 'deploy:cleanup'
+end
+
+namespace :composer do
+
+  desc 'Composer install vendors'
+  task :install do
+    on roles(:app) do
+      within release_path do
+        info execute(:composer, '-n', 'install')
+      end
+    end
+  end
+
+  desc 'Composer update vendors'
+  task :update do
+    on roles(:app) do
+      within release_path do
+        info execute(:composer, '-n', 'update')
+      end
+    end
+  end
+
 end
 
