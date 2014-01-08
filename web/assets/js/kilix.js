2f15013915c4fb310c81ba46aefd376d1edcead3
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2014-01-08 18:17:43 +0100

    Fixed blank space, added  links to footer nav links

--- web/assets/js/kilix.js
+++ web/assets/js/kilix.js
@@ -56,7 +56,7 @@ var Kilix = {
                     $(".wrapper:first-child").remove();
                     $(".wrapper-new").attr('style', '').removeClass('wrapper-new');
                     $(".wrapper-prev").attr('style', '').removeClass('wrapper-prev');
-                    $(".nav-links-wrapper a").addClass('enabled');
+                    $(".nav-links-wrapper a, .footer-links a").addClass('enabled');
                     Pos = $(".container").data('pos');
                 });
 
@@ -79,12 +79,12 @@ var Kilix = {
             updateContent(State);
         });
 
-        $(".nav-links-wrapper a.enabled").on("click", function(evt) {
+        $(".nav-links-wrapper a.enabled, .footer-links a.enabled").on("click", function(evt) {
             evt.preventDefault();
             if(!$(this).hasClass('enabled')) {
                 return;
             }
-            $(".nav-links-wrapper a").removeClass('enabled');
+            $(".nav-links-wrapper a, .footer-links a").removeClass('enabled');
             $('body').removeClass('unfolded');
             //Prevent the browsers default behaviour of navigating to the hyperlink
             currentPos = $(".nav-link.current").data('pos');
