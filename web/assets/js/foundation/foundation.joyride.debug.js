cdb6123988fa2aa5976c84e5c0e258c4f0dcf83d
RaDyUm <romr09@gmail.com>
2014-01-16 19:06:47 +0000

    adding arrow in a fucking rotated square

---
+++ web/assets/js/foundation/foundation.joyride.debug.js
@@ -0,0 +1,14 @@
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 60, col 37, Missing semicolon.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 119, col 11, Confusing use of '!'.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 142, col 80, Missing semicolon.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 224, col 9, Bad line breaking before '||'.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 442, col 28, 'leftOffset' is already defined.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 601, col 38, Use '===' to compare with 'null'.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 603, col 54, Missing radix parameter.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 703, col 23, Missing semicolon.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 731, col 14, Wrap the /regexp/ literal in parens to disambiguate the slash operator.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 735, col 14, Wrap the /regexp/ literal in parens to disambiguate the slash operator.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 739, col 14, Wrap the /regexp/ literal in parens to disambiguate the slash operator.
+C:\Vagrant\ftv-tops\www\kilix-v2\web\assets\js\foundation\foundation.joyride.js: line 743, col 14, Wrap the /regexp/ literal in parens to disambiguate the slash operator.
+
+12 errors
