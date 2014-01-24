7975c4d271f54b2c624562e4aeb1144d7add1d19
Clément Seiller <cseiller@kilix.fr>
2014-01-24 16:44:29 +0000

    refacto agility-animations / rename agility svgs

--- web/assets/js/animation-agilite.js
+++ web/assets/js/animation-agilite.js
@@ -4,215 +4,166 @@ Kilix.animations["agilite"] = {
     Kilix.animations["agilite"].animStatus = status;
   },
 
-  start: function () {
-      var valSVG = Snap("#AGI-value"),
-          riskSVG = Snap("#AGI-risk"),
-          elementsValue = new Array(),
-          elementsRisk = new Array(),
-          delay = 50,
-          duration = 500;
+  initAnimElements: function(elements, svg, scales, callback) {
+    for (var element in elements) {
+      elements[element].attr({'opacity': 0});
+      svg.append(elements[element]);
+    }
 
-      function loadValSvg() {
+    scales.animate({'opacity':1}, 500);
 
-        Snap.load("./assets/img/svg/agi1.svg", function(f) {
-          full = f.select("#full"),
-          elementsValue['valueScales'] = f.select("#v-scales"),
-          elementsValue['valuePointilles1'] = f.select("#v-pointilles1"),
-          elementsValue['valuePointilles2'] = f.select("#v-pointilles2"),
-          elementsValue['valueCourbeBleue'] = f.select("#v-courbe-bleue"),
-          elementsValue['valueOldGris'] = f.select("#v-old-gris"),
-          elementsValue['valuePointBleu'] = f.select("#v-point-bleu"),
-          elementsValue['valueTime'] = f.select("#v-time"),
-          elementsValue['valuePlan'] = f.select("#v-plan"),
-          elementsValue['valuePlan2'] = f.select("#v-plan-2"),
-          elementsValue['valueText'] = f.select("#v-valeur");
-
-          for (var element in elementsValue) {
-              elementsValue[element].attr({'opacity': 0});
-              valSVG.append(elementsValue[element]);
-          }
-
-          elementsValue['valueScales'].animate({'opacity':1}, 500);
-
-        });
+    if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+      for (var element in elements) {
+        elements[element].animate({'opacity': 1}, 500);
       }
+    } else {
+      setTimeout(callback(), 400);
+    }
+  },
 
-      function valAnimation() {
-        showCheckmark($('.agility-item:eq(0)'));
-        scale(elementsValue['valuePlan'], 1, 500, 200, function(){});
-        scale(elementsValue['valuePlan2'], 1, 500, 200, function() {
-          drawPath(valSVG, elementsValue['valueOldGris'], 900, 1200, mina.easeout, function(){
-            drawPath(valSVG, elementsValue['valueCourbeBleue'], 780, 800, mina.easeinout, function(){
-              alterBounce(elementsValue['valuePointBleu'], 400, 400, function() {
-                elementsValue['valueText'].animate({opacity:1} ,400 );
-                alterBounce(elementsValue['valueTime'], 600, 400, function() {
-                  drawLine(valSVG, elementsValue['valuePointilles1'], 500, 700, mina.easeout, function(){
-                    drawLine(valSVG, elementsValue['valuePointilles2'], 500, 1200, mina.easeout, function(){
-                      riskAnimation();
-                    });
-                  });
+  startValueAnimation: function () {
+    var svg = Snap("#AGI-value"),
+        elements = new Array();
+
+    function loadSvg() {
+
+      Snap.load("./assets/img/svg/agiValue.svg", function(f) {
+        elements['scales'] = f.select("#v-scales"),
+        elements['pointilles1'] = f.select("#v-pointilles1"),
+        elements['pointilles2'] = f.select("#v-pointilles2"),
+        elements['courbeBleue'] = f.select("#v-courbe-bleue"),
+        elements['oldGris'] = f.select("#v-old-gris"),
+        elements['pointBleu'] = f.select("#v-point-bleu"),
+        elements['time'] = f.select("#v-time"),
+        elements['plan'] = f.select("#v-plan"),
+        elements['plan2'] = f.select("#v-plan-2"),
+        elements['text'] = f.select("#v-valeur");
+
+        Kilix.animations["agilite"].initAnimElements(elements, svg, elements['scales'], valAnimation);
+
+      });
+    }
+
+    function valAnimation() {
+      scale(elements['plan'], 1, 500, 200, function(){});
+      scale(elements['plan2'], 1, 500, 200, function() {
+        drawPath(svg, elements['oldGris'], 900, 1200, mina.easeout, function(){
+          drawPath(svg, elements['courbeBleue'], 780, 800, mina.easeinout, function(){
+            alterBounce(elements['pointBleu'], 400, 400, function() {
+              elements['text'].animate({opacity:1} ,400 );
+              alterBounce(elements['time'], 600, 400, function() {
+                drawLine(svg, elements['pointilles1'], 500, 700, mina.easeout, function(){
+                  drawLine(svg, elements['pointilles2'], 500, 1200, mina.easeout);
                 });
               });
             });
           });
         });
-      }
+      });
+    }
 
-      function loadRiskSvg(callback) {
-
-        Snap.load("./assets/img/svg/agi2.svg", function(f) {
-          elementsRisk['riskScales'] = f.select("#r-scales"),
-          elementsRisk['riskCourbeBleue'] = f.select("#r-courbe-bleue"),
-          elementsRisk['riskOldGris'] = f.select("#r-old-gris"),
-          elementsRisk['riskTime'] = f.select("#r-time"),
-           elementsRisk['riskPlan'] = f.select("#r-plan"),
-           elementsRisk['riskPlan2'] = f.select("#r-plan-2"),
-          elementsRisk['riskText'] = f.select("#r-valeur");
-
-          for (var element in elementsRisk) {
-              elementsRisk[element].attr({'opacity': 0});
-              riskSVG.append(elementsRisk[element]);
-          }
-
-          elementsRisk['riskScales'].animate({'opacity':1}, 500, function() {
-            callback();
-          });
-
-        });
-      }
-
-      function riskAnimation() {
-        showCheckmark($('.agility-item:eq(1)'));
-        scale(elementsRisk['riskPlan'], 1, 500, 200, function(){});
-        scale(elementsRisk['riskPlan2'], 1, 500, 200, function() {
-           drawPath(riskSVG, elementsRisk['riskOldGris'], 900, 1200, mina.easeout, function(){
-             drawPath(riskSVG, elementsRisk['riskCourbeBleue'], 780, 800, mina.easeinout, function(){
-                elementsRisk['riskText'].animate({opacity:1} ,400 );
-                  alterBounce(elementsRisk['riskTime'], 600, 400, function() {
-                });
-             });
-           });
-         });
-      }
-
-    loadValSvg();
-    loadRiskSvg(function() {
-      if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-
-        showCheckmark($('.agility-item:eq(0)'));
-        for (var element in elementsValue) {
-          elementsValue[element].animate({'opacity': 1}, 500);
-        }
-
-        showCheckmark($('.agility-item:eq(1)'));
-        for (var element in elementsRisk) {
-            elementsRisk[element].animate({'opacity': 1}, 500);
-        }
-      } else {
-        setTimeout(function(){valAnimation()}, 600);
-      }
-    });
-
+    loadSvg();
   },
 
-  startSecond: function() {
-    var   adaptSVG = Snap("#AGI-adapt"),
-          visSVG = Snap("#AGI-vis"),
-          elementsAdapt = new Array(),
-          elementsVis = new Array(),
-          delay = 50,
-          duration = 500;
+  startRiskAnimation: function() {
+    var svg = Snap("#AGI-risk"),
+        elements = new Array();
 
+    function loadSvg() {
 
-      function loadAdaptabilitySvg() {
+      Snap.load("./assets/img/svg/agiRisk.svg", function(f) {
+        elements['scales'] = f.select("#r-scales"),
+        elements['courbeBleue'] = f.select("#r-courbe-bleue"),
+        elements['oldGris'] = f.select("#r-old-gris"),
+        elements['time'] = f.select("#r-time"),
+        elements['plan'] = f.select("#r-plan"),
+        elements['plan2'] = f.select("#r-plan-2"),
+        elements['text'] = f.select("#r-valeur");
 
-        Snap.load("./assets/img/svg/agi3.svg", function(f) {
-          elementsAdapt['adaptScales'] = f.select("#a-scales"),
-          elementsAdapt['adaptCourbeBleue'] = f.select("#a-courbe-bleue"),
-          elementsAdapt['adaptOldGris'] = f.select("#a-old-gris"),
-          elementsAdapt['adaptTime'] = f.select("#a-time"),
-          elementsAdapt['adaptPlan'] = f.select("#a-plan"),
-          elementsAdapt['adaptPlan2'] = f.select("#a-plan-2"),
-          elementsAdapt['adaptText'] = f.select("#a-valeur");
+        Kilix.animations["agilite"].initAnimElements(elements, svg, elements['scales'], riskAnimation);
+      });
+    }
 
-          for (var element in elementsAdapt) {
-              elementsAdapt[element].attr({'opacity': 0});
-              adaptSVG.append(elementsAdapt[element]);
-          }
-
-          elementsAdapt['adaptScales'].animate({'opacity':1}, 500, function() {});
-
-        });
-      }
-
-      function adaptabilityAnimation() {
-        showCheckmark($('.agility-item:eq(2)'));
-        scale(elementsAdapt['adaptPlan'], 1, 500, 200, function(){});
-        scale(elementsAdapt['adaptPlan2'], 1, 500, 200, function() {
-           drawPath(adaptSVG, elementsAdapt['adaptOldGris'], 900, 1200, mina.easeout, function(){
-             drawPath(adaptSVG, elementsAdapt['adaptCourbeBleue'], 780, 800, mina.easeinout, function(){
-                elementsAdapt['adaptText'].animate({opacity:1} ,400 );
-                  alterBounce(elementsAdapt['adaptTime'], 600, 400, function() {
-                    visibilityAnimation();
-                });
-             });
+    function riskAnimation() {
+      scale(elements['plan'], 1, 500, 200, function(){});
+      scale(elements['plan2'], 1, 500, 200, function() {
+         drawPath(svg, elements['oldGris'], 900, 1200, mina.easeout, function(){
+           drawPath(svg, elements['courbeBleue'], 780, 800, mina.easeinout, function(){
+              elements['text'].animate({opacity:1}, 400);
+              alterBounce(elements['time'], 600, 400);
            });
          });
-      }
+       });
+    }
 
-      function loadVisibilitySvg(callback) {
+    loadSvg();
+  },
 
-        Snap.load("./assets/img/svg/agi4.svg", function(f) {
-          elementsVis['visScales'] = f.select("#vis-scales"),
-          elementsVis['visCourbeBleue'] = f.select("#vis-courbe-bleue"),
-          elementsVis['visOldGris'] = f.select("#vis-old-gris"),
-          elementsVis['visTime'] = f.select("#vis-time"),
-           elementsVis['visPlan'] = f.select("#vis-plan"),
-           elementsVis['visPlan2'] = f.select("#vis-plan-2"),
-          elementsVis['visText'] = f.select("#vis-valeur");
+  startAdaptAnimation: function() {
+    var svg = Snap("#AGI-adapt"),
+        elements = new Array();
 
-          for (var element in elementsVis) {
-              elementsVis[element].attr({'opacity': 0});
-              visSVG.append(elementsVis[element]);
-          }
+    function loadSvg() {
 
-          elementsVis['visScales'].animate({'opacity':1}, 500, callback());
+      Snap.load("./assets/img/svg/agiAdaptability.svg", function(f) {
+        elements['scales'] = f.select("#a-scales"),
+        elements['courbeBleue'] = f.select("#a-courbe-bleue"),
+        elements['oldGris'] = f.select("#a-old-gris"),
+        elements['time'] = f.select("#a-time"),
+        elements['plan'] = f.select("#a-plan"),
+        elements['plan2'] = f.select("#a-plan-2"),
+        elements['text'] = f.select("#a-valeur");
 
+        Kilix.animations["agilite"].initAnimElements(elements, svg, elements['scales'], adaptabilityAnimation);
+      });
+    }
+
+    function adaptabilityAnimation() {
+      scale(elements['plan'], 1, 500, 200, function(){});
+      scale(elements['plan2'], 1, 500, 200, function() {
+        drawPath(svg, elements['oldGris'], 900, 1200, mina.easeout, function(){
+          drawPath(svg, elements['courbeBleue'], 780, 800, mina.easeinout, function(){
+            elements['text'].animate({opacity:1}, 400);
+            alterBounce(elements['time'], 600, 400);
+          });
         });
-      }
+      });
+    }
 
-      function visibilityAnimation() {
-        showCheckmark($('.agility-item:eq(3)'));
-        scale(elementsVis['visPlan'], 1, 500, 200, function(){});
-        scale(elementsVis['visPlan2'], 1, 500, 200, function() {
-           drawPath(visSVG, elementsVis['visOldGris'], 900, 1200, mina.easeout, function(){
-             drawPath(visSVG, elementsVis['visCourbeBleue'], 780, 800, mina.easeinout, function(){
-                elementsVis['visText'].animate({opacity:1} ,400 );
-                  alterBounce(elementsVis['visTime'], 600, 400, function() {
-                });
-             });
-           });
-         });
-      }
+    loadSvg();
+  },
 
-    loadAdaptabilitySvg();
-    loadVisibilitySvg(function() {
+  startVisAnimation: function() {
+    var svg = Snap("#AGI-vis"),
+        elements = new Array();
 
-      if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    function loadSvg() {
 
-        showCheckmark($('.agility-item:eq(2)'));
-        for (var element in elementsAdapt) {
-          elementsAdapt[element].animate({'opacity': 1}, 500);
-        }
+      Snap.load("./assets/img/svg/agiVisibility.svg", function(f) {
+        elements['scales'] = f.select("#vis-scales"),
+        elements['courbeBleue'] = f.select("#vis-courbe-bleue"),
+        elements['oldGris'] = f.select("#vis-old-gris"),
+        elements['time'] = f.select("#vis-time"),
+        elements['plan'] = f.select("#vis-plan"),
+        elements['plan2'] = f.select("#vis-plan-2"),
+        elements['text'] = f.select("#vis-valeur");
 
-        showCheckmark($('.agility-item:eq(3)'));
-        for (var element in elementsVis) {
-            elementsVis[element].animate({'opacity': 1}, 500);
-        }
-      } else {
-        setTimeout(function(){adaptabilityAnimation()}, 600);
-      }
-    });
-    
+        Kilix.animations["agilite"].initAnimElements(elements, svg, elements['scales'], visibilityAnimation);
+      });
+    }
+
+    function visibilityAnimation() {
+      scale(elements['plan'], 1, 500, 200, function(){});
+      scale(elements['plan2'], 1, 500, 200, function() {
+        drawPath(svg, elements['oldGris'], 900, 1200, mina.easeout, function(){
+          drawPath(svg, elements['courbeBleue'], 780, 800, mina.easeinout, function(){
+            elements['text'].animate({opacity:1}, 400);
+            alterBounce(elements['time'], 600, 400);
+          });
+        });
+      });
+    }
+
+    loadSvg();
   }
 }
