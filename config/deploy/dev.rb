25b61155ee8b76960114db2521671fff11e2a23c
Sylvain Arrambourg <sarrambourg@kilix.fr>
2014-08-06 20:01:07 +0200

    Change dev stage

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -4,6 +4,6 @@ set :stage, :dev
 
 role :app, %w{deploy@dev.kilix-v2.kilix.net}
 
-set :ssh_options, proxy: Net::SSH::Proxy::Command.new('ssh deploy@reverse.dev.kilix.net -W %h:%p')
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new('ssh deploy@dev.bastion.kilix.net -W %h:%p')
 
 set :branch, ENV['REVISION'] || 'develop'
