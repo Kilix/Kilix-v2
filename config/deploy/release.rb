a2dbecdd329c640e431fe611fb337ecc2e0b6551
Sylvain Arrambourg <sarrambourg@kilix.fr>
2014-08-07 14:29:30 +0200

    Change release stage

--- config/deploy/release.rb
+++ config/deploy/release.rb
@@ -4,6 +4,6 @@ set :stage, :release
 
 role :app, %w{deploy@release.kilix-v2.kilix.net}
 
-set :ssh_options, proxy: Net::SSH::Proxy::Command.new('ssh deploy@reverse.release.kilix.net -W %h:%p')
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new('ssh deploy@release.bastion.kilix.net -W %h:%p')
 
 set :branch, ENV['REVISION'] || `git branch -r | grep -oE release/+[0-9].?+[0-9] | tr -d "\n"`
