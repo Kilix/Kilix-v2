d5ea98ce0331ae85801d4de0a3524b00a3a22e3a
William Castandet <wcastandet@kilix.fr>
2016-01-26 17:27:39 +0000

    fix animation timing and easing

--- web/assets/js/animation-references.js
+++ web/assets/js/animation-references.js
@@ -14,7 +14,7 @@ Kilix.animations["references"] = {
     var pictures = $(p).find('.pictures');
     $(p).find('.pictures .maquette-ref').velocity('stop').velocity(
       { opacity: 1, skewX: ['25deg', '25deg'], translateY: '0%' },
-      { duration: 500, delay: 750, ease: 'ease-out',
+      { duration: 1000, delay: 950, easing: [0.215, 0.610, 0.355, 1.000],
       begin: function(el){
         $(pictures).css('overflow', 'visible')
       }
@@ -22,21 +22,21 @@ Kilix.animations["references"] = {
 
     $(p).find('.ref-btn').velocity('stop').velocity(
       {width: '50px'},
-      {duration: 300, delay: 750, ease: 'ease-out'});
+      {duration: 500, delay: 1250, easing: [0.550, 0.055, 0.675, 0.190]});
     $(p).find('.ref-btn-mobile').velocity('stop').velocity(
       {opacity: 0},
-      {duration:500, delay: 750, ease: 'ease-out'});
+      {duration:500, delay: 950, easing: [0.215, 0.610, 0.355, 1.000]});
   },
   close: function(p){
     var pictures = $(p).find('.pictures');
-    $(p).find('.ref-btn').velocity('stop').velocity({width: '0px'}, 300);
+    $(p).find('.ref-btn').velocity('stop').velocity({width: '0px'}, {duration:500, easing: [0.550, 0.055, 0.675, 0.190]});
     $(p).find('.pictures .maquette-ref').velocity('stop').velocity(
       { opacity: 0, skewX: ['25deg', '25deg'], translateY: '-20%' },
-      { duration: 800, delay: 0, ease: 'ease-out',
+      { duration: 850, delay: 450, easing: [0.215, 0.610, 0.355, 1.000],
       complete: function(el){
         $(pictures).css('overflow', 'hidden')
       }
     })
-    $(p).find('.ref-btn-mobile').velocity('stop').velocity({opacity: 1}, 500);
+    $(p).find('.ref-btn-mobile').velocity('stop').velocity({opacity: 1}, {duration: 500, easing: [0.215, 0.610, 0.355, 1.000]});
   }
 }
