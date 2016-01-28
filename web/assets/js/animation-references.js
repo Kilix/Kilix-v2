aa26d569119126c316fab2ef7ecc78716b39c3d0
William Castandet <wcastandet@kilix.fr>
2016-01-28 09:30:41 +0000

    tweak animation timing

--- web/assets/js/animation-references.js
+++ web/assets/js/animation-references.js
@@ -11,10 +11,11 @@ Kilix.animations["references"] = {
       })
   },
   open: function(p){
+    var easing = [.55,0,.1,1];
     var pictures = $(p).find('.pictures');
     $(p).find('.pictures .maquette-ref').velocity('stop').velocity(
       { opacity: 1, skewX: ['25deg', '25deg'], translateY: '0%' },
-      { duration: 1000, delay: 950, easing: [0.215, 0.610, 0.355, 1.000],
+      { duration: 1000, delay: 950, easing: easing,
       begin: function(el){
         $(pictures).css('overflow', 'visible')
       }
@@ -22,21 +23,22 @@ Kilix.animations["references"] = {
 
     $(p).find('.ref-btn').velocity('stop').velocity(
       {width: '70px'},
-      {duration: 500, delay: 1250, easing: [0.550, 0.055, 0.675, 0.190]});
+      {duration: 500, delay: 1850, easing: easing});
     $(p).find('.ref-btn-mobile').velocity('stop').velocity(
       {opacity: 0},
-      {duration:500, delay: 950, easing: [0.215, 0.610, 0.355, 1.000]});
+      {duration:500, delay: 950, easing: easing});
   },
   close: function(p){
+    var easing = [.55,0,.1,1];
     var pictures = $(p).find('.pictures');
-    $(p).find('.ref-btn').velocity('stop').velocity({width: '0px'}, {duration:500, easing: [0.550, 0.055, 0.675, 0.190]});
+    $(p).find('.ref-btn').velocity('stop').velocity({width: '0px'}, {duration:300, easing: easing});
     $(p).find('.pictures .maquette-ref').velocity('stop').velocity(
       { opacity: 0, skewX: ['25deg', '25deg'], translateY: '-20%' },
-      { duration: 850, delay: 450, easing: [0.215, 0.610, 0.355, 1.000],
+      { duration: 500, delay: 250, easing: easing,
       complete: function(el){
         $(pictures).css('overflow', 'hidden')
       }
     })
-    $(p).find('.ref-btn-mobile').velocity('stop').velocity({opacity: 1}, {duration: 500, easing: [0.215, 0.610, 0.355, 1.000]});
+    $(p).find('.ref-btn-mobile').velocity('stop').velocity({opacity: 1}, {duration: 500, easing: easing});
   }
 }
