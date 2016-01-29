862f476ffb625b05cd2225939f90ba3817e987f3
William Castandet <wcastandet@kilix.fr>
2016-01-29 15:39:12 +0000

    save work before reboot

--- web/assets/js/animation-references.js
+++ web/assets/js/animation-references.js
@@ -1,13 +1,23 @@
+var easingIn  = [.42, 0, 1, 1];
+var easingOut = [0, 0,.58, 1];
+
 Kilix.animations["references"] = {
   init:function(){
     $('.ref').hover(function(e){
+      var el = e.currentTarget;
       var pictures = $(el).find('.logo-ref');
       var logo = $(el).find('.logo-ref');
       var maquette = $(el).find('.maquette-ref');
+      var infos = $(el).find('.infos').children();
+
+      logo.velocity('stop');
+      maquette.velocity('stop');
+      infos.velocity('stop');
 
       var seq = [
-        { e: logo, $:{'transition.slideUpBigOut'}, o: { duration: 800, delay: 0, easing: [.55,0,.1,1], display: 'block'} },
-        { e: maquette, $:{'transition.slideUpBigOut'}, o: { duration: 800, delay: 0, easing: [.55,0,.1,1], display: 'block'} }
+        { e: logo, p: 'transition.slideUpBigOut', o: { duration: 650, easing: easingIn, display: 'block'} },
+        { e: maquette, p: 'transition.slideUpBigOut', o: { duration: 650, delay: 300, easing: easingIn, display: 'block', sequenceQueue: false } },
+        { e: infos, p: 'transition.slideUpBigIn', o: { duration: 500, delay: 400, easing: easingIn, display: 'block', stagger: 150, sequenceQueue: false } },
       ];
 
       $.Velocity.RunSequence(seq);
@@ -16,10 +26,18 @@ Kilix.animations["references"] = {
       var el = e.currentTarget;
       var logo = $(el).find('.logo-ref');
       var maquette = $(el).find('.maquette-ref');
+      var infos = $(el).find('.infos').children();
+
+      logo.velocity('stop');
+      maquette.velocity('stop');
+      infos.velocity('stop');
+
       var seq = [
-        { e: logo, $:{'transition.slideDownBigIn'}, o: { duration: 800, delay: 0, easing: [.55,0,.1,1], display: 'block'} },
-        { e: maquette, $:{'transition.slideDownBigIn'}, o: { duration: 800, delay: 0, easing: [.55,0,.1,1], display: 'block'} }
+        { e: infos, p: 'transition.slideDownBigOut', o: { duration: 500, easing: easingIn, display: 'block', backwards: true, stagger: 150} },
+        { e: maquette, p: 'transition.slideDownBigIn', o: { duration: 650, delay: 250, easing: easingOut, display: 'block', sequenceQueue: false} },
+        { e: logo, p: 'transition.slideDownBigIn', o: { duration: 650, delay: 300, easing: easingOut, display: 'block', sequenceQueue: false } },
       ];
+      $.Velocity.RunSequence(seq);
     })
   }
 }
